c68159f6f8829279cb587e3b698e532ee1889495
chenmeng <2691357857@qq.com>
2023-12-03 17:55:55 +0800

    添加http测试文件，方遍使用HttpClient工具

---
+++ member/src/main/java/com/chenmeng/train/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.chenmeng.train.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试控制器
+ *
+ * @author 沉梦
+ * @date 2023/12/03 17:46
+ **/
+@RestController("/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String test() {
+        return "Hello World!";
+    }
+
+}
