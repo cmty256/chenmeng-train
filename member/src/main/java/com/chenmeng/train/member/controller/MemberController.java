48636b29a81a28b142da9c110445c496a595e3c8
chenmeng <2691357857@qq.com>
2023-12-06 15:11:35 +0800

    编写登录接口

--- member/src/main/java/com/chenmeng/train/member/controller/MemberController.java
+++ member/src/main/java/com/chenmeng/train/member/controller/MemberController.java
@@ -1,8 +1,10 @@
 package com.chenmeng.train.member.controller;
 
 import com.chenmeng.train.common.resp.CommonResp;
+import com.chenmeng.train.member.model.dto.MemberLoginReq;
 import com.chenmeng.train.member.model.dto.MemberRegisterReq;
 import com.chenmeng.train.member.model.dto.MemberSendCodeReq;
+import com.chenmeng.train.member.model.vo.MemberLoginVO;
 import com.chenmeng.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
@@ -49,5 +51,16 @@ public class MemberController {
     public CommonResp<Long> sendCode(@Valid @RequestBody MemberSendCodeReq req) {
         memberService.sendCode(req);
         return new CommonResp<>();
+    }
+
+    /**
+     * 登录
+     * @param req
+     * @return
+     */
+    @PostMapping("/login")
+    public CommonResp<MemberLoginVO> login(@Valid @RequestBody MemberLoginReq req) {
+        MemberLoginVO resp = memberService.login(req);
+        return new CommonResp<>(resp);
     }
 }
--- member/src/main/java/com/chenmeng/train/member/service/MemberService.java
+++ member/src/main/java/com/chenmeng/train/member/service/MemberService.java
@@ -1,15 +1,19 @@
 package com.chenmeng.train.member.service;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.chenmeng.train.common.exception.BusinessException;
 import com.chenmeng.train.common.exception.BusinessExceptionEnum;
+import com.chenmeng.train.common.util.JwtUtil;
 import com.chenmeng.train.common.util.SnowUtil;
 import com.chenmeng.train.member.mapper.MemberMapper;
+import com.chenmeng.train.member.model.dto.MemberLoginReq;
 import com.chenmeng.train.member.model.dto.MemberRegisterReq;
 import com.chenmeng.train.member.model.dto.MemberSendCodeReq;
 import com.chenmeng.train.member.model.entity.Member;
 import com.chenmeng.train.member.model.entity.MemberExample;
+import com.chenmeng.train.member.model.vo.MemberLoginVO;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,6 +31,11 @@ import java.util.List;
 public class MemberService {
 
     private static final Logger LOG = LoggerFactory.getLogger(MemberService.class);
+
+    /**
+     * 固定短信验证码，方便测试用
+     */
+    private static final String TEST_CODE = "8888";
 
     @Resource
     private MemberMapper memberMapper;
@@ -65,9 +74,8 @@ public class MemberService {
             LOG.info("手机号存在，不插入记录");
         }
 
-        // 生成验证码
-        // String code = RandomUtil.randomString(4);
-        String code = "8888";
+        // 生成验证码 -- 实际应该用随机数 -- RandomUtil.randomString(4)
+        String code = TEST_CODE;
         LOG.info("生成短信验证码：{}", code);
 
         // 保存短信记录表：手机号，短信验证码，有效期，是否已使用，业务类型，发送时间，使用时间
@@ -75,6 +83,27 @@ public class MemberService {
 
         // 对接短信通道，发送短信
         LOG.info("对接短信通道");
+    }
+
+    public MemberLoginVO login(MemberLoginReq req) {
+        String mobile = req.getMobile();
+        String code = req.getCode();
+        Member memberDB = selectByMobile(mobile);
+
+        // 如果手机号不存在，则报：请先获取短信验证码，来注册手机号
+        if (ObjectUtil.isNull(memberDB)) {
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_NOT_EXIST);
+        }
+
+        // 校验短信验证码，实际应该从数据库中取，而不是 8888
+        if (!TEST_CODE.equals(code)) {
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_CODE_ERROR);
+        }
+
+        MemberLoginVO memberLoginResp = BeanUtil.copyProperties(memberDB, MemberLoginVO.class);
+        String token = JwtUtil.createToken(memberLoginResp.getId(), memberLoginResp.getMobile());
+        memberLoginResp.setToken(token);
+        return memberLoginResp;
     }
 
     private Member selectByMobile(String mobile) {
