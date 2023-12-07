c786ba591bc074329788878d77c30d95cda3779d
chenmeng <2691357857@qq.com>
2023-12-07 17:19:07 +0800

    增添注释, 并记录一个todo

--- common/src/main/java/com/chenmeng/train/common/util/JwtUtil.java
+++ common/src/main/java/com/chenmeng/train/common/util/JwtUtil.java
@@ -24,6 +24,12 @@ public class JwtUtil {
      */
     private static final String key = "ChenMeng12306";
 
+    /**
+     * 生成 token
+     * @param id
+     * @param mobile
+     * @return
+     */
     public static String createToken(Long id, String mobile) {
         LOG.info("开始生成JWT token，id：{}，mobile：{}", id, mobile);
         GlobalBouncyCastleProvider.setUseBouncyCastle(false);
@@ -44,20 +50,45 @@ public class JwtUtil {
         return token;
     }
 
-    public static boolean validate(String token) {
+    /**
+     * 校验 token
+     * 成功逻辑: 这个 token 没有被篡改过，且还在有效期内
+     *
+     * @param token
+     * @return true 或者 false
+     */
+   public static boolean validate(String token) {
         LOG.info("开始JWT token校验，token：{}", token);
+        // 设置使用BouncyCastle为false，不使用BouncyCastle
         GlobalBouncyCastleProvider.setUseBouncyCastle(false);
-        JWT jwt = JWTUtil.parseToken(token).setKey(key.getBytes());
+        // 解析token
+        JWT jwt = JWTUtil
+                .parseToken(token)
+                .setKey(key.getBytes());
         // validate包含了verify
         boolean validate = jwt.validate(0);
         LOG.info("JWT token校验结果：{}", validate);
         return validate;
     }
 
-    public static JSONObject getJSONObject(String token) {
+    /**
+     * 根据 token 获取原始内容 payloads
+     * 工具类一般不依赖项目具体的某个类，所以写成通用的json
+     *
+     * @param token
+     * @return
+     */
+   public static JSONObject getJSONObject(String token) {
+       // todo 增加校验, 校验为 false 时不能获取到原始内容 -- validate(String token)
+        // 设置使用BouncyCastle为false
         GlobalBouncyCastleProvider.setUseBouncyCastle(false);
-        JWT jwt = JWTUtil.parseToken(token).setKey(key.getBytes());
+        // 解析token
+        JWT jwt = JWTUtil
+                .parseToken(token)
+                .setKey(key.getBytes());
+        // 获取payload
         JSONObject payloads = jwt.getPayloads();
+        // 移除时间戳
         payloads.remove(JWTPayload.ISSUED_AT);
         payloads.remove(JWTPayload.EXPIRES_AT);
         payloads.remove(JWTPayload.NOT_BEFORE);
@@ -65,6 +96,10 @@ public class JwtUtil {
         return payloads;
     }
 
+    /**
+     * 工具类方法测试
+     * @param args
+     */
     public static void main(String[] args) {
         createToken(1L, "123");
 
