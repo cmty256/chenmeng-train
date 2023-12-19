809254a8be40d148b78e7fcb31431077b7fc7208
chenmeng <2691357857@qq.com>
2023-12-19 21:20:54 +0800

    添加车站表, 并利用Mybatis官方插件生成持久层代码

---
+++ business/src/main/java/com/chenmeng/train/business/mapper/StationMapper.java
@@ -0,0 +1,30 @@
+package com.chenmeng.train.business.mapper;
+
+import com.chenmeng.train.business.model.entity.Station;
+import com.chenmeng.train.business.model.entity.StationExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface StationMapper {
+    long countByExample(StationExample example);
+
+    int deleteByExample(StationExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Station record);
+
+    int insertSelective(Station record);
+
+    List<Station> selectByExample(StationExample example);
+
+    Station selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Station record, @Param("example") StationExample example);
+
+    int updateByExample(@Param("record") Station record, @Param("example") StationExample example);
+
+    int updateByPrimaryKeySelective(Station record);
+
+    int updateByPrimaryKey(Station record);
+}
