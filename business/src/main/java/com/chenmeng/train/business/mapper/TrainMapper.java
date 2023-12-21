c87c6161b6ae21213af7522d230ac097423e1929
chenmeng <2691357857@qq.com>
2023-12-21 16:25:30 +0800

    添加train车次表，生成Mybatis持久层代码

---
+++ business/src/main/java/com/chenmeng/train/business/mapper/TrainMapper.java
@@ -0,0 +1,30 @@
+package com.chenmeng.train.business.mapper;
+
+import com.chenmeng.train.business.model.entity.Train;
+import com.chenmeng.train.business.model.entity.TrainExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TrainMapper {
+    long countByExample(TrainExample example);
+
+    int deleteByExample(TrainExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Train record);
+
+    int insertSelective(Train record);
+
+    List<Train> selectByExample(TrainExample example);
+
+    Train selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Train record, @Param("example") TrainExample example);
+
+    int updateByExample(@Param("record") Train record, @Param("example") TrainExample example);
+
+    int updateByPrimaryKeySelective(Train record);
+
+    int updateByPrimaryKey(Train record);
+}
