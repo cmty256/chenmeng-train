57e5b5e5a03fd68bb0f6024a56c958739720dfc9
chenmeng <2691357857@qq.com>
2024-01-22 11:52:06 +0800

    hello接口添加test前缀

--- member/src/main/java/com/chenmeng/train/member/controller/TestController.java
+++ member/src/main/java/com/chenmeng/train/member/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.chenmeng.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @author 沉梦听雨
  * @date 2023/12/03 17:46
  **/
-@RestController("/test")
+@RestController
+@RequestMapping("/test")
 public class TestController {
 
     @GetMapping("/hello")
