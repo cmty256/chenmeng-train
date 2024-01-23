d42dc7a227b9eaa1dca8a897ae8dfb66e2a596ba
chenmeng <2691357857@qq.com>
2024-01-23 20:54:01 +0800

    为余票信息页面增加查询条件，并优化页面展示

--- business/src/main/java/com/chenmeng/train/business/model/dto/DailyTrainTicketQueryDTO.java
+++ business/src/main/java/com/chenmeng/train/business/model/dto/DailyTrainTicketQueryDTO.java
@@ -1,16 +1,39 @@
 package com.chenmeng.train.business.model.dto;
 
 import com.chenmeng.train.common.req.PageReq;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 /**
  * @author 沉梦听雨
  **/
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class DailyTrainTicketQueryDTO extends PageReq {
 
-    @Override
-    public String toString() {
-        return "DailyTrainTicketQueryDTO{" +
-                "} " + super.toString();
-    }
+    /**
+     * 日期
+     */
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    /**
+     * 车次编号
+     */
+    private String trainCode;
+
+    /**
+     * 出发站
+     */
+    private String start;
+
+    /**
+     * 到达站
+     */
+    private String end;
+
 }
 
--- business/src/main/java/com/chenmeng/train/business/service/DailyTrainTicketService.java
+++ business/src/main/java/com/chenmeng/train/business/service/DailyTrainTicketService.java
@@ -5,6 +5,7 @@ import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.EnumUtil;
+import cn.hutool.core.util.ObjUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.chenmeng.train.business.enums.SeatTypeEnum;
 import com.chenmeng.train.business.enums.TrainTypeEnum;
@@ -67,11 +68,20 @@ public class DailyTrainTicketService {
     public PageResp<DailyTrainTicketQueryVO> queryList(DailyTrainTicketQueryDTO req) {
         // 创建一个 DailyTrainTicketExample 对象
         DailyTrainTicketExample dailyTrainTicketExample = new DailyTrainTicketExample();
-        // 实现 id 降序
-        dailyTrainTicketExample.setOrderByClause("id desc");
-
-        // 创建一个 DailyTrainTicketExample.Criteria 对象（类似 MP 的条件构造器）
+        dailyTrainTicketExample.setOrderByClause("`date` desc, start_time asc, train_code asc, `start_index` asc, `end_index` asc");
         DailyTrainTicketExample.Criteria criteria = dailyTrainTicketExample.createCriteria();
+        if (ObjUtil.isNotNull(req.getDate())) {
+            criteria.andDateEqualTo(req.getDate());
+        }
+        if (ObjUtil.isNotEmpty(req.getTrainCode())) {
+            criteria.andTrainCodeEqualTo(req.getTrainCode());
+        }
+        if (ObjUtil.isNotEmpty(req.getStart())) {
+            criteria.andStartEqualTo(req.getStart());
+        }
+        if (ObjUtil.isNotEmpty(req.getEnd())) {
+            criteria.andEndEqualTo(req.getEnd());
+        }
 
         LOG.info("查询页码：{}", req.getPage());
         LOG.info("每页条数：{}", req.getSize());
