1acf3a0da387104c3fc796ea1182fdf768e74462
chenmeng <2691357857@qq.com>
2024-02-25 21:18:17 +0800

    配置Gateway+Nacos，实现按应用名来做路由转发(相同应用名，会轮询访问)

--- member/src/main/java/com/chenmeng/train/member/controller/TestController.java
+++ member/src/main/java/com/chenmeng/train/member/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.chenmeng.train.member.controller;
 
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +22,12 @@ public class TestController {
     @Value("${test.nacos:Nacos}")
     private String testNacos;
 
+    @Resource
+    private Environment environment;
+
     @GetMapping("/hello")
-    public String test() {
-        return "Hello World!" + " - " + testNacos;
+    public String hello() {
+        String port = environment.getProperty("local.server.port");
+        return String.format("Hello %s! 端口：%s", testNacos, port);
     }
 }
