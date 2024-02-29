79c3afcd73357ad2267ed8054d1e717da68d4d5a
chenmeng <2691357857@qq.com>
2024-02-29 15:50:12 +0800

    引入SpringBoot内置的缓存依赖，开启使用缓存注解

--- business/src/main/java/com/chenmeng/train/BusinessApplication.java
+++ business/src/main/java/com/chenmeng/train/BusinessApplication.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.core.env.Environment;
 
@@ -14,6 +15,7 @@ import org.springframework.core.env.Environment;
 @SpringBootApplication
 @EnableFeignClients("com.chenmeng.train.business.fegin")
 @MapperScan("com.chenmeng.train.*.mapper")
+@EnableCaching
 public class BusinessApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
